7c937faeacd7902c617f903204c4bd34556ade8b
KINGBUTTS\chris <c>
2025-08-10 02:00:24 +1200

    Build created - Physics settings adjusted and bail resolution fixed

---
+++ Assets/Scripts/PhysicsParityBootstrap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PhysicsParityBootstrap : MonoBehaviour
+{
+    public int targetFps = 120; // or 144; vSync off gives you headroom
+    public int vSync = 0;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = vSync;
+        Application.targetFrameRate = targetFps;
+
+        Time.fixedDeltaTime = 0.004f;   // 250 Hz physics
+        Time.maximumDeltaTime = 0.048f; // limit catch-up (≈12 steps max)
+
+        Physics2D.velocityIterations = 12;
+        Physics2D.positionIterations = 6;
+    }
+}
